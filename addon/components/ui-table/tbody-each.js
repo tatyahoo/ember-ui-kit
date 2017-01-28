faecca6eb787a264b23a92c72aebfcab8f5cd1a2
Ming Liu <vmliu1@gmail.com>
2017-01-28 11:05:17 -0800

    Add support for Select microstate

--- addon/components/ui-table/tbody-each.js
+++ addon/components/ui-table/tbody-each.js
@@ -14,6 +14,22 @@ export default Body.extend({
   model: null,
   // attrs }
 
+  modelNormalized: Ember.computed('model', function() {
+    let content = this.get('model');
+
+    if (!Ember.isArray(content) && typeof content.forEach === 'function') {
+      let accum = [];
+
+      content.forEach(item => {
+        accum.push(item);
+      });
+
+      content = accum;
+    }
+
+    return Ember.ArrayProxy.create({ content });
+  }).readOnly(),
+
   buffer: construct(Ember.A).readOnly(),
   bufferCursors: Ember.computed(function() {
     return { start: 0, end: 0 };
@@ -32,9 +48,9 @@ export default Body.extend({
 
     let cursor = this.get('bufferCursors');
     let buffer = this.get('buffer');
-    let model = this.get('model');
-    let bufLen = Ember.get(buffer, 'length');
-    let modLen = Ember.get(model, 'length');
+    let model = this.get('modelNormalized');
+    let bufLen = buffer.get('length');
+    let modLen = model.get('length');
 
     let table = this.get('table.measurements.height') || 0;
 
@@ -42,19 +58,21 @@ export default Body.extend({
     let target = Math.floor(table / tr);
 
     if (isNaN(target)) {
-      buffer.pushObject({
+      buffer.pushObject(Ember.Object.create({
+        guid: Ember.generateGuid(this),
         tr: [],
         index: bufLen,
-        model: model[bufLen] || model.objectAt(bufLen)
-      });
+        model: model.objectAt(bufLen)
+      }));
     }
     else if (target - bufLen > 0) {
       for (let index = buffer.get('length'); target - index; index = buffer.get('length')) {
-        buffer.pushObject({
+        buffer.pushObject(Ember.Object.create({
+          guid: Ember.generateGuid(this),
           tr: [],
           index: index,
-          model: model[index] || model.objectAt(index)
-        });
+          model: model.objectAt(index)
+        }));
 
         cursor.end++;
       }
@@ -83,6 +101,14 @@ export default Body.extend({
       }
     },
 
+    select: {
+      render() {
+        this.addObserver('model.[]', () => {
+          this.$().trigger('scroll');
+        });
+      }
+    },
+
     scroll: {
       render() {
         let scroller = this.get('scroller').get(0);
@@ -94,7 +120,7 @@ export default Body.extend({
           let buffer = this.get('buffer');
           let bufLen = buffer.get('length');
           let cursors = this.get('bufferCursors');
-          let model = this.get('model');
+          let model = this.get('modelNormalized');
 
           let direction = evt.target.scrollTop - lastScrollTop;
 
@@ -136,9 +162,8 @@ export default Body.extend({
 
               cursors.end++;
               Ember.run(Ember, Ember.setProperties, buffer.objectAt(cursors.start % bufLen), {
-                tr: [],
                 index: cursors.end,
-                model: model[cursors.end] || model.objectAt(cursors.end)
+                model: model.objectAt(cursors.end)
               });
               cursors.start++;
 
@@ -153,13 +178,19 @@ export default Body.extend({
 
               cursors.start--;
               Ember.run(Ember, Ember.setProperties, buffer.objectAt(cursors.end % bufLen), {
-                tr: [],
                 index: cursors.start,
-                model: model[cursors.start] || model.objectAt(cursors.start)
+                model: model.objectAt(cursors.start)
               });
               cursors.end--;
 
               continue;
+            }
+
+            if (direction === 0) {
+              // refresh models right at the cursor
+              Ember.run(buffer, buffer.forEach, item => {
+                item.set('model', model.objectAt(item.index));
+              });
             }
 
             break;
