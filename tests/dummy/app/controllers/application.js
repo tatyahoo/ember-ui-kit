d9de7c3ed03d3dfa0e207511342008c5c5980498
Ming Liu <vmliu1@gmail.com>
2017-02-01 19:05:44 -0800

    Fix mirage data

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  ajax: Ember.inject.service(),
+
+  model() {
+    return this.get('ajax').request('/users').then(resp => {
+      return resp.data;
+    });
+  }
 });
--- tests/dummy/app/controllers/ui-table.js
+++ tests/dummy/app/controllers/ui-table.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['mode'],
 
+  application: Ember.inject.controller(),
+
   mode: null,
 
   actions: {
