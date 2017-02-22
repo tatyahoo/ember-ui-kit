07274235dc52a30a94238558fd37e5b0ccde12eb
Ming Liu <vmliu1@gmail.com>
2017-02-21 16:20:13 -0800

    Improve doc

---
+++ addon/index.js
@@ -0,0 +1,25 @@
+/**
+ * The `form` module consists of components that deals with form
+ * and validation. The `form` module components are separate
+ * from `input` module to provide validation.
+ *
+ * @module form
+ * @main
+ */
+
+/**
+ * The `input` module consists of components that deals with
+ * obtaining user inputs. The components themselves does not
+ * provide validation, but integrates with `form` module
+ * components for that functionality
+ *
+ * @module input
+ * @main
+ */
+
+/**
+ * The `ui` module consists are lower level components.
+ *
+ * @module ui
+ * @main
+ */
