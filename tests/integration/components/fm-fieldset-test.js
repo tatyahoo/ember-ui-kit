ca099b7db5c5e47c69de0d17cdee049f51cfad23
Ming Liu <vmliu1@gmail.com>
2017-02-23 09:01:24 -0800

    Add form fieldset

---
+++ tests/integration/components/fm-fieldset-test.js
@@ -0,0 +1,43 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import { validator, buildValidations } from 'ember-cp-validations';
+import hbs from 'htmlbars-inline-precompile';
+
+import Ember from 'ember';
+import Microstates from 'ember-microstates/initializers/microstates';
+
+moduleForComponent('fm-fieldset', 'Integration | Component | fm-fieldset', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  Microstates.initialize(this);
+
+  this.register('model:user', DS.Model.extend({
+    name: DS.attr('string'),
+    ssn: DS.attr('number')
+  }).reopen(buildValidations({
+    name: {
+      description: 'Name',
+      validators: [ validator('presence', true) ]
+    }
+  })));
+
+  this.inject.service('store');
+
+  this.set('model', Ember.run(this.store, this.store.createRecord, 'user', {
+    name: null,
+    ssn: null
+  }));
+
+  this.render(hbs`
+    {{#fm-form model}}
+      {{#fm-fieldset}}
+      {{/fm-fieldset}}
+      {{#fm-fieldset}}
+      {{/fm-fieldset}}
+    {{/fm-form}}
+  `);
+
+    assert.equal(this.$('.fm-form').length, 1, 'should render 1 form');
+    assert.equal(this.$('.fm-fieldset').length, 2, `should render 2 field set`);
+});
