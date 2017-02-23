1601a843795cbdc96be1b19188d047ae24a68a93
Ming Liu <vmliu1@gmail.com>
2017-02-23 10:01:20 -0800

    Release v0.4.0 docs

--- docs/api.js
+++ docs/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "fm-field",
+        "fm-fieldset",
         "fm-form",
         "in-text",
         "in-toggle",
