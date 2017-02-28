8c45b913aa6ab6e524159c0d56e2592d6608fb84
Ming Liu <vmliu1@gmail.com>
2017-02-27 18:01:56 -0800

    Refactor component for AST transform

--- addon/components/fm-field.js
+++ addon/components/fm-field.js
@@ -16,55 +16,16 @@ export default Ember.Component.extend(Composable, {
   componentRegistrationName: 'field',
   tagName: 'label',
   classNames: 'fm-field',
-  attributeBindings: 'modelAttribute:data-model-attribute',
+  classNameBindings: [
+    'validation.isValid:fm-field--valid:',
+    'validation.isInvalid:fm-field--invalid'
+  ],
+  attributeBindings: 'valuePath:data-model-attribute',
   layout,
 
   // attrs {
-  model: null,
+  value: null,
+  validPath: null,
+  validation: null,
   // attrs }
-
-  modelValue: Ember.computed('isModelValidatable', 'model.value', function() {
-    if (this.get('isModelValidatable')) {
-      return this.get('model.value');
-    }
-
-    return this.get('model');
-  }),
-
-  modelAttribute: Ember.computed('isModelValidatable', 'model.results.attribute', function() {
-    if (this.get('isModelValidatable')) {
-      return this.get('model.results.attribute');
-    }
-
-    return null;
-  }).readOnly(),
-
-  modelValidation: Ember.computed(function() {
-    if (this.get('isModelValidatable')) {
-      return this.get('model.results');
-    }
-
-    return null;
-  }).readOnly(),
-
-  isRequired: Ember.computed('isModelValidatable', 'model.results.options.presence', function() {
-    return Boolean(this.get('isModelValidatable') && this.get('model.results.options.presence'));
-  }).readOnly(),
-
-  isModelValidatable: Ember.computed('model', function() {
-    return this.get('model') instanceof Validatable;
-  }).readOnly(),
-
-  actions: {
-    modelValueDidChange(newValue) {
-      if (this.get('isModelValidatable')) {
-        this.get('model').update(newValue);
-      }
-      else {
-        MS.set(this, 'model', newValue);
-      }
-    }
-  }
-}).reopenClass({
-  positionalParams: ['model']
 });
