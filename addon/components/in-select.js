dd9b5f021853f1115e827f02b4428782ad687d80
Ming Liu <vmliu1@gmail.com>
2017-03-05 18:35:54 -0800

    Add base select component

---
+++ addon/components/in-select.js
@@ -0,0 +1,136 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+import layout from '../templates/components/in-select';
+
+export default Ember.Component.extend({
+  classNames: 'in-select',
+  layout,
+
+  /**
+   * The selected option. The select is multi select if `value` is bound
+   * an array-like object.
+   *
+   * `value` makes no garantee about object identity correctness. This means
+   * `value` may contain value that does not match `from` content by strict
+   * equal `===`. `value` does garantee the object match by configured `key`.
+   *
+   * @attribute value
+   */
+  value: null,
+
+  /**
+   * @attribute from
+   */
+  from: null,
+
+  /**
+   * @attribute key
+   */
+  key: null,
+
+  multiple: Ember.computed('value', function() {
+    return Ember.isArray(this.get('value'));
+  }).readOnly(),
+
+  list: Ember.computed('from', function() {
+    let from = this.get('from');
+
+    if (typeof from === 'function') {
+      from = from();
+    }
+
+    return DS.PromiseArray.create({
+      promise: Ember.RSVP.resolve(from)
+    });
+  }).readOnly(),
+
+  isSelected: Ember.computed('key', 'value.[]', function() {
+    let key = this.get('key');
+    let value = this.get('value');
+
+    if (!Ember.isArray(value)) {
+      value = [ value ];
+    }
+
+    value = Ember.A(value);
+
+    if (key) {
+      value = value.mapBy(key);
+
+      return item => {
+        return value.includes(Ember.get(item, key));
+      };
+    }
+    else {
+      return item => {
+        return value.includes(item);
+      };
+    }
+  }).readOnly(),
+
+  actions: {
+    select(item) {
+      if (this.get('multiple')) {
+        let key = this.get('key');
+        let value = this.get('value');
+
+        let exists = key ?
+          Ember.A(value).findBy(key, Ember.get(item, key)) :
+          Ember.A(value).includes(item);
+
+        if (!exists) {
+          if (value.pushObject) {
+            value.pushObject(item);
+          }
+          else {
+            value.push(item);
+          }
+        }
+      }
+      else {
+        this.set('value', item);
+      }
+    },
+
+    unselect(item) {
+      if (this.get('multiple')) {
+        let key = this.get('key');
+        let value = this.get('value');
+
+        if (value.removeObject) {
+          if (key) {
+            value.removeObject(value.findBy(key, Ember.get(item, key)));
+          }
+          else {
+            value.removeObject(item);
+          }
+        }
+        else {
+          if (key) {
+            value.splice(value.indexOf(Ember.A(value).findBy(key, Ember.get(item, key))), 1);
+          }
+          else {
+            value.splice(value.indexOf(item), 1);
+          }
+
+        }
+      }
+      else {
+        this.set('value', null);
+      }
+    },
+
+    toggle(item) {
+      let check = this.get('isSelected');
+
+      if (check(item)) {
+        this.send('unselect', item);
+      }
+      else {
+        this.send('select', item);
+      }
+    }
+  }
+}).reopenClass({
+  positionalParams: ['value']
+});
---
+++ tests/unit/components/in-select-test.js
@@ -0,0 +1,229 @@
+import { moduleForComponent, test } from 'ember-qunit';
+
+moduleForComponent('in-select', 'Unit | Component | in-select', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true
+});
+
+test('#multiple', function(assert) {
+  let subject = this.subject();
+
+  let multiple = [
+    [],
+    Ember.A(),
+    Ember.ArrayProxy.create({})
+  ];
+
+  let single = [
+    undefined,
+    null,
+    1,
+    'abc',
+    {},
+    true,
+    false
+  ];
+
+  multiple.forEach(check => {
+    subject.set('value', check);
+
+    assert.equal(subject.get('multiple'), true);
+  });
+
+  single.forEach(check => {
+    subject.set('value', check);
+
+    assert.equal(subject.get('multiple'), false);
+  });
+});
+
+test('#list', async function(assert) {
+  let subject = this.subject();
+  let content = [
+    'hello',
+    'world'
+  ];
+
+  let checks = Ember.run(function() {
+    return [ content, Ember.A(content), Ember.ArrayProxy.create({ content }) ]
+      .reduce((accum, check) => {
+        let promise = Ember.RSVP.resolve(check);
+
+        return accum.concat([ check ], function() { return check; }, promise, function() { return promise; });
+      }, []);
+  });
+
+  while (checks.length) {
+    subject.set('from', checks.shift());
+
+    assert.deepEqual((await subject.get('list')).toArray(), content);
+  }
+});
+
+test('#isSelected', function(assert) {
+  let subject = this.subject();
+
+  subject.set('value', 1)
+
+  assert.equal(subject.get('isSelected')(1), true);
+  assert.equal(subject.get('isSelected')(2), false);
+
+  subject.set('value', false)
+
+  assert.equal(subject.get('isSelected')(false), true);
+  assert.equal(subject.get('isSelected')(true), false);
+
+  subject.set('value', [ 1 ])
+
+  assert.equal(subject.get('isSelected')(1), true);
+  assert.equal(subject.get('isSelected')(2), false);
+
+  subject.set('value', [ false ])
+
+  assert.equal(subject.get('isSelected')(false), true);
+  assert.equal(subject.get('isSelected')(true), false);
+
+  subject.set('key', 'id');
+
+  subject.set('value', { id: 1 })
+
+  assert.equal(subject.get('isSelected')({ id: 1 }), true);
+  assert.equal(subject.get('isSelected')({ id: 2 }), false);
+
+  subject.set('value', { id: false })
+
+  assert.equal(subject.get('isSelected')({ id: false }), true);
+  assert.equal(subject.get('isSelected')({ id: true }), false);
+
+  subject.set('value', [ { id: 1 } ])
+
+  assert.equal(subject.get('isSelected')({ id: 1 }), true);
+  assert.equal(subject.get('isSelected')({ id: 2 }), false);
+
+  subject.set('value', [ { id: false } ])
+
+  assert.equal(subject.get('isSelected')({ id: false }), true);
+  assert.equal(subject.get('isSelected')({ id: true }), false);
+});
+
+test('@select', function(assert) {
+  let subject = this.subject();
+
+  subject.setProperties({
+    value: null,
+    key: 'id',
+    from: [
+      {
+        id: 1,
+        name: 'Link'
+      },
+      {
+        id: 2,
+        name: 'Zelda'
+      }
+    ]
+  });
+
+  subject.send('select', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), { id: 1 });
+
+  subject.setProperties({
+    value: [],
+    key: 'id',
+    from: [
+      {
+        id: 1,
+        name: 'Link'
+      },
+      {
+        id: 2,
+        name: 'Zelda'
+      }
+    ]
+  });
+
+  subject.send('select', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), [ { id: 1 } ]);
+
+  subject.send('select', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), [ { id: 1 } ]);
+
+  subject.send('select', { id: 2 });
+
+  assert.deepEqual(subject.get('value'), [ { id: 1 }, { id: 2 } ]);
+});
+
+test('@unselect', function(assert) {
+  let subject = this.subject();
+
+  subject.setProperties({
+    value: { id: 1 },
+    key: 'id',
+    from: [
+      {
+        id: 1,
+        name: 'Link'
+      },
+      {
+        id: 2,
+        name: 'Zelda'
+      }
+    ]
+  });
+
+  assert.deepEqual(subject.get('value'), { id: 1 });
+
+  subject.send('unselect', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), null);
+
+  subject.setProperties({
+    value: [ { id: 1 } ],
+    key: 'id',
+    from: [
+      {
+        id: 1,
+        name: 'Link'
+      },
+      {
+        id: 2,
+        name: 'Zelda'
+      }
+    ]
+  });
+
+  subject.send('unselect', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), []);
+
+  subject.setProperties({
+    value: [ { id: 1 }, { id: 2 } ],
+    key: 'id',
+    from: [
+      {
+        id: 1,
+        name: 'Link'
+      },
+      {
+        id: 2,
+        name: 'Zelda'
+      }
+    ]
+  });
+
+  subject.send('unselect', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), [ { id: 2 } ]);
+
+  subject.send('unselect', { id: 1 });
+
+  assert.deepEqual(subject.get('value'), [ { id: 2 } ]);
+
+  subject.send('unselect', { id: 2 });
+
+  assert.deepEqual(subject.get('value'), []);
+});
