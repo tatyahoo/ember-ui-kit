20ae3b99a97a18600b6a57f402d46639fba00dd9
Ming Liu <vmliu1@gmail.com>
2017-03-05 19:00:50 -0800

    Convert to eslint

--- tests/unit/components/in-select-test.js
+++ tests/unit/components/in-select-test.js
@@ -1,5 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 
+import Ember from 'ember';
+
 moduleForComponent('in-select', 'Unit | Component | in-select', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar'],
