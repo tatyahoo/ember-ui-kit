1e968d55c62921580cd415959486d8378cdcccec
Ming Liu <vmliu1@gmail.com>
2017-03-09 16:12:36 -0800

    Fix firefox at 51

--- testem.js
+++ testem.js
@@ -3,6 +3,9 @@ module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
+  "query_params": {
+    "dockcontainer": true
+  },
   "launch_in_ci": [
     "Firefox"
   ],
