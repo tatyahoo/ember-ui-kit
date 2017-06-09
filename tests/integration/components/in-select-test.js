8bcc887cf7232c0551ea342f1b9fe33774f6dcb6
Ming Liu <vmliu1@gmail.com>
2017-06-09 10:14:26 -0700

    Add test case for chain binding

--- tests/integration/components/in-select-test.js
+++ tests/integration/components/in-select-test.js
@@ -58,3 +58,39 @@ test('selecting triggers change event', async function(assert) {
 
   assert.ok(spy.called);
 });
+
+test('linked lists can be cleared with help', async function(assert) {
+  this.set('array1', [
+    {
+      name: 'L',
+      array2: [
+        { name: 'K' },
+        { name: 'A' }
+      ]
+    },
+    {
+      name: 'Z',
+      array2: [
+        { name: 'N' },
+        { name: 'D' }
+      ]
+    }
+  ]);
+  this.set('value1', this.get('array1')[0]);
+  this.set('value2', this.get('array1')[0].array2[0]);
+
+  this.render(hbs`
+    {{in-select key="name" from=array1 value=value1 labelPath="name"}}
+    {{in-select key="name" from=value1.array2 value=value2 labelPath="name"}}
+
+    <div>{{value1.name}} {{value2.name}}</div>
+  `);
+
+  assert.equal(this.$('.in-select:nth(0) li').toArray().map(el => el.textContent.trim()).join(), 'L,Z')
+  assert.equal(this.$('div:last').text(), 'L K');
+
+  await click('.in-select:nth-child(1) li:nth-child(2)');
+
+  assert.equal(this.$('.in-select:nth(1) li').toArray().map(el => el.textContent.trim()).join(), 'N,D')
+  assert.equal(this.$('div:last').text().trim(), 'Z');
+});
